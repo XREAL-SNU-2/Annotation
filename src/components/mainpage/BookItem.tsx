b9c44616b65917f71efeef4be9928dc0d0fe2690
Soongyu <sungyu1291@yonsei.ac.kr>
2022-08-17 23:35:28 +0900

    [feat] Select PDF at MainPage

--- src/components/mainpage/BookItem.tsx
+++ src/components/mainpage/BookItem.tsx
@@ -4,16 +4,22 @@ import { Input } from 'web3uikit';
 import BookInfo from './BookInfo';
 import Modal from './Modal';
 import './BookItem.scss';
-
+import useStore from "../../pages/Zustand";
 type book = {
   name: string;
 };
 
 const BookItem = ({ name }: book) => {
   const [isOpenModal, setOpenModal] = useState<boolean>(false);
-  const onClickToggleModal = useCallback(() => {
+  
+  const {pdfFileName, setPdfFileName} = useStore();
+  
+  const onClickToggleModal = useCallback(async() => {
+    await setPdfFileName(name);
+    console.log(pdfFileName);
     setOpenModal(!isOpenModal);
   }, [isOpenModal]);
+
   return (
     <>
       <div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction} from 'react';
 import { Link } from 'react-router-dom';
 import { TextArea, Icon } from 'web3uikit';
 import { useState, useCallback } from 'react';
 import TopicList from '../components/mainpage/TopicList';
 import BookList from '../components/mainpage/BookList';
 import './MainPage.scss';
+
 
 const Mainpage = () => {
   return (

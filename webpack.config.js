95b35cb3156d32a59ab9a4a4c1e41f19dfa37d3f
tnsqjahong <cruseight47@naver.com>
2022-08-20 02:03:23 +0900

    [fix] webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,61 @@
 module.exports = {
-    mode: "development",
-    entry: "./src/index.tsx",
-    output: {
-        filename: "index.js",
-        path: __dirname + "/dist",
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/dist',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    fallback: {
+      os: require.resolve('os-browserify/browser'),
+      https: require.resolve('https-browserify'),
+      http: require.resolve('stream-http'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
     },
-    devtool: "source-map",
-    resolve: {
-        extension: [".ts", ".tsx", ".js"],
-    },
-    module: {
-        rules: [
-            {test: /\.tsx?$/, loader: "ts-loader"},
-        ],
-    },
-    devServer: {
-        port: 8080,
-        contentBase: './',
-        publicPath: '/dist'
-    }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './',
+    publicPath: '/dist',
+  },
+};
